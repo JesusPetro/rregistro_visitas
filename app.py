ae859974f8583002163dc8de98585baf0934a90c
carlos agamez <oruz@gmail.com>
2022-04-15 14:20:20 -0500

    creacion de plantillas y rutas para el registro de los bisitantes y de las visitas

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-model = []
+
+# settings
+app.secret_key = "mysecretkey"
 
 # pagina de inicio
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/add_visitante")
+# registro de visitas
+@app.route("/add_visita", methods=['POST'])
+def add_visita():
+    e_mail = request.form['e-mail']
+    dia = request.form['dia']
+    flash('Visita registrada satisfactoriamente')
+    return redirect(url_for('index'))
+
+# pasamos a asolicitar los datos del visitante 
+@app.route("/info_visitante")
+def info_visitante():
+    return render_template('info_visitante.html')
+
+
+# añadimos los visitantes a la base de datos 
+@app.route("/add_visitante", methods=['POST'])
 def add_visitante():
-    return render_template('add_visitante.html')
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    e_mail = request.form['e-mail']
+    tipo = request.form['tipo']
+    placa = request.form['placa']
+    descripcion = request.form['descripcion']
+    flash('Visitante agregado satisfactoriamente')
+    return redirect(url_for('index'))
 """
 @app.route('/person_detail', methods=['POST'])
 def person_detail():
